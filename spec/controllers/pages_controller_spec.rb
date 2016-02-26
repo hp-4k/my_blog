ed3230306d4d4f9c035fb58dce79cc939c95c91a
Hubert <hpompecki@gmail.com>
2016-02-26 16:03:43 +0000

    rewrite controller spec to use 'expect' syntax

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 RSpec.describe HighVoltage::PagesController, type: :controller do
   
-  context "on GET to /pages/home" do
+  describe "GET to /pages/home" do
     
     before do
       get :show, id: 'home'
     end
-
-    it { should respond_with(:success) }
-    it { should render_template(:home) }
+    
+    it "renders the 'home' template" do 
+      expect(response.status).to eq 200
+      expect(response).to render_template(:home)
+    end
     
   end
   
